54c9aab11e7cd37c6f1286827b9eac88df174d25
Muhammet Kus <77937107+muhametkus@users.noreply.github.com>
2024-08-07 17:04:16 +0300

    Update Harita.jsx

--- client/src/components/IletisimComponents/Harita.jsx
+++ client/src/components/IletisimComponents/Harita.jsx
@@ -6,7 +6,7 @@ const Harita = () => {
       <div className="w-1/2 sm:w-full ">
         <h3 className="text-orange-500 text-2xl text-center mb-3">Showroom Adresimiz</h3>
         <div className="w-full">
-        <iframe className="w-full h-[400px]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d533.3019015503298!2d29.264646142432674!3d40.876018926369994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cadd2d14a5d4c7%3A0x36c1eabdd575254c!2zSGViaWxvxJ9sdSBBaMWfYXAgTWHEn2F6YQ!5e0!3m2!1str!2str!4v1702758005243!5m2!1str!2str"  style={{border: 0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+        <iframe className="w-full h-[400px]" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d266.65129805211575!2d29.264813650294172!3d40.87593270967828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14caddce5113572b%3A0x70923d2fefe4cca9!2zSGViaWxvxJ9sdSBBaMWfYXAgxLDDpyBLYXDEsSBNYcSfYXphc8Sx!5e0!3m2!1str!2str!4v1723039414529!5m2!1str!2str"  style={{border: 0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
         </div>
       </div>
       <div className="w-1/2 sm:w-full  ">
